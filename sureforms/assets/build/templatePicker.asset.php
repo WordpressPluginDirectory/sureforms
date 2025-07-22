0eaa0fe0afe8d4ecb111f1377e9afcb90361bb18
Justin Gardner <justin@rhynorater.com>
2025-07-22 06:04:32 -0500

    1.9.0

--- sureforms/assets/build/templatePicker.asset.php
+++ sureforms/assets/build/templatePicker.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => '090112fd414da6745e37');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => '0f5918df3ee7a51be9d4');
