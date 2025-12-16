01367c714720037e53bce4d63e8793fdce287792
Justin Gardner <justin@rhynorater.com>
2025-12-16 06:03:17 -0500

    2.2.0

--- sureforms/assets/build/entries.asset.php
+++ sureforms/assets/build/entries.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => 'b29d01c2db8848143138');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => '933d85de2a2b39ef4841');
